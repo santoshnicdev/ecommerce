9767e8cac331908db070d25adb1a4561e8cd917d
santoshnicdev <60836180+santoshnicdev@users.noreply.github.com>
2024-02-23 10:51:46 +0530

    Update web.php
    web from office

--- ecommerce/routes/web.php
+++ ecommerce/routes/web.php
@@ -1,21 +1,11 @@
 <?php
 
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get("/india",[ProductController::class,'india']);
 
 Route::view("/","login");
-Route::post("/log",[UserController::class,"login"]);
-Route::get("/logged",[ProductController::class,"ind"]);
+Route::post("/login", [UserController::class,"login"]);
